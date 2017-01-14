fd861a33d50a3a29e3bf57a3e32dceb31872ab23
Mathias Petermann <mathias.petermann@gmail.com>
2017-01-14 20:52:02 +0000

    Update requirements

--- temp_hum_uploader.py
+++ temp_hum_uploader.py
@@ -2,14 +2,18 @@
 
 # import libraties used in script
 import sys
-
 import Adafruit_DHT
+import urlparse
+import requests
 
 # Type of sensor, can be Adafruit_DHT.DHT11, Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
 DHT_TYPE = Adafruit_DHT.DHT22
  
 # Sensor connected to Beaglebone Black pin P8_11
 DHT_PIN  = 'P8_11'
+
+REST_API_BASE = 'http://10.112.10.15:5000'
+REST_API_ENDPOINT = '/temp-hum-list/'
 
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
@@ -21,6 +25,7 @@ humidity, temperature = Adafruit_DHT.read_retry(DHT_TYPE, DHT_PIN)
 # If this happens try again!
 if humidity is not None and temperature is not None:
     print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+    r = request.post(urlparse.urljoin(REST_API_BASE,REST_API_ENDPOINT), data={'temp': temperature, 'hum': humidity})
 else:
     print('Failed to get reading. Try again!')
     sys.exit(1)
