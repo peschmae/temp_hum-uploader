d2e3f791df5846894a98990810be074c097ac083
Mathias Petermann <mathias.petermann@gmail.com>
2017-01-14 20:42:56 +0000

    Add requirements.txt and temp_hum_uploader

---
+++ temp_hum_uploader.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+# import libraties used in script
+import sys
+
+import Adafruit_DHT
+
+# Type of sensor, can be Adafruit_DHT.DHT11, Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
+DHT_TYPE = Adafruit_DHT.DHT22
+ 
+# Sensor connected to Beaglebone Black pin P8_11
+DHT_PIN  = 'P8_11'
+
+# Try to grab a sensor reading.  Use the read_retry method which will retry up
+# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+humidity, temperature = Adafruit_DHT.read_retry(DHT_TYPE, DHT_PIN)
+
+# Note that sometimes you won't get a reading and
+# the results will be null (because Linux can't
+# guarantee the timing of calls to read the sensor).
+# If this happens try again!
+if humidity is not None and temperature is not None:
+    print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+else:
+    print('Failed to get reading. Try again!')
+    sys.exit(1)
